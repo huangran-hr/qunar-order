126884d5f59c2a92b43754648ba71cf2d26647d2
huangran <huangran@jdjt.net>
2020-11-17 10:20:05 +0800

    commit

--- src/main/java/com/sjlh/hotel/order/controller/PersonOrderController.java
+++ src/main/java/com/sjlh/hotel/order/controller/PersonOrderController.java
@@ -1,32 +1,32 @@
-///**
-// *
-// */
-//package com.sjlh.hotel.order.controller;
-//
-//import javax.annotation.Resource;
-//
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.sjlh.hotel.order.dto.PersonDto;
-//import com.sjlh.hotel.order.service.PersonService;
-//
-///**
-// * @author Administrator
-// *
-// */
-//@RestController()
-//public class PersonOrderController {
-//	@Resource
-//	private PersonService personService;
-//
-//	@RequestMapping("/person/get")
-//	@ResponseBody
-//	public PersonDto getPerson() {
-//		PersonDto p = new PersonDto();
-//		p.setId(100);
-//		PersonDto person = personService.getPerson(p);
-//		return person;
-//	}
-//}
+/**
+ *
+ */
+package com.sjlh.hotel.order.controller;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.sjlh.hotel.order.dto.PersonDto;
+import com.sjlh.hotel.order.service.PersonService;
+
+/**
+ * @author Administrator
+ *
+ */
+@RestController()
+public class PersonOrderController {
+	@Resource
+	private PersonService personService;
+
+	@RequestMapping("/person/get")
+	@ResponseBody
+	public PersonDto getPerson() {
+		PersonDto p = new PersonDto();
+		p.setId(100);
+		PersonDto person = personService.getPerson(p);
+		return person;
+	}
+}
