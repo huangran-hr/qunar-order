81635d87ed5233c17c60f800bc92b823b3429ca6
huangran <huangran@jdjt.net>
2020-11-27 15:35:43 +0800

    去掉eureka

--- src/main/java/com/sjlh/hotel/order/QunarOrderApplication.java
+++ src/main/java/com/sjlh/hotel/order/QunarOrderApplication.java
@@ -7,7 +7,6 @@ import com.sjlh.hotel.crs.configurate.EnableCrsOrderClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.sjlh.qunar.client.spring.EnableQunarClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
