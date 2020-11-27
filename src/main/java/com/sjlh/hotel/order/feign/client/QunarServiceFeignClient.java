a78e59f88419304e486bc7ea5bd189685ba833c1
huangran <huangran@jdjt.net>
2020-11-27 14:48:49 +0800

    增加hotel-qunar-api 服务名配置

--- src/main/java/com/sjlh/hotel/order/feign/client/QunarServiceFeignClient.java
+++ src/main/java/com/sjlh/hotel/order/feign/client/QunarServiceFeignClient.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @Date 2020/11/17 10:41
  * @Description:
  */
-@FeignClient(name="qunar-service")
+@FeignClient(name="${hotel-qunar-api-name}")
 public interface QunarServiceFeignClient {
     /**
      * 获取产品信息
