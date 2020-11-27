ab4a22bce42e9bf8b82b9f46c29e38963a09c81c
huangran <huangran@jdjt.net>
2020-11-27 10:50:05 +0800

    修改镜像名

--- src/main/java/com/sjlh/hotel/order/kafka/service/KafkaProducerService.java
+++ src/main/java/com/sjlh/hotel/order/kafka/service/KafkaProducerService.java
@@ -11,6 +11,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+@SuppressWarnings("unchecked")
 @Service
 public class KafkaProducerService {
 
